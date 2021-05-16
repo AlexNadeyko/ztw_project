082214a316b9d080aedd4a23a4c553a300353462
Alex <nadeykoa@gmail.com>
2021-05-16 22:09:41 +0200

    Initialized spring boot project

---
+++ SportsHallReservationAPI/src/main/java/pl/edu/pwr/ztw/SportsHallReservationAPI/SportsHallReservationApiApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.ztw.SportsHallReservationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsHallReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsHallReservationApiApplication.class, args);
+	}
+
+}
---
+++ SportsHallReservationAPI/src/test/java/pl/edu/pwr/ztw/SportsHallReservationAPI/SportsHallReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.ztw.SportsHallReservationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsHallReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
