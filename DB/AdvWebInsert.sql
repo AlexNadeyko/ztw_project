27ecc55772ae2c4c42bbf00cd6698234eec60752
Alex <nadeykoa@gmail.com>
2021-05-17 09:46:03 +0200

    Fixed database scripts

--- DB/AdvWebInsert.sql
+++ DB/AdvWebInsert.sql
@@ -1,28 +1,30 @@
 
-INSERT INTO Users([Name],[Surname],[Mail],[Password],[BirthDate])
+
+USE AdvWeb;
+
+INSERT INTO Users(Name,Surname,Mail,Password,BirthDate)
 VALUES ('Hleb', 'Liaonik', '248795@student.pwr.edu.pl', 'hleb123','2000-02-04'),
 		('Aliaksandr', 'Nadzeika', 'xxxxxx@student.pwr.edu.pl', 'alek123', NULL),
 		('Test', 'Test', 'Test', 'Test', NULL);
 
-INSERT INTO [dbo].[HallType]([Type])
+INSERT INTO HallType(Type)
 VALUES ('Basketball'),
 		('Tennis'),
 		('Baseball'),
 		('Golf'),
-		('Volleyball')
+		('Volleyball');
 
-INSERT INTO [dbo].[SportHalls]([Type],[WorkFrom],[WorkTo],[PriceOneHour],[Capacity],[NrHall])
+INSERT INTO SportHalls(Type,WorkFrom,WorkTo,PriceOneHour,Capacity,NrHall)
 VALUES (2,'07:00','20:00',25.0,20,3),
 		(2,'07:00','20:00',25.0,NULL,4),
 		(2,'07:00','20:00',25.0,25,5),
 		(3,'08:00','20:00',20.0,NULL,3),
 		(1,'08:00','20:00',15.0,NULL,3),
-		(4,'08:00','20:00',25.0,20,3)
+		(4,'08:00','20:00',25.0,20,3);
 
-INSERT INTO [dbo].[Rentals]([IdUser],[IdHall],[RentalDate],[TimeFrom],[TimeTo],[FullPrice],[Paid],[Description])
+INSERT INTO Rentals(IdUser,IdHall,RentalDate,TimeFrom,TimeTo,FullPrice,Paid,Description)
 VALUES (2,1,'2021-05-18','11:00','13:00',45,1,NULL),
 	   (3,2,'2021-05-28','09:00','10:00',25,0,NULL),
-	   (4,3,'2021-05-17','08:00','09:00',30,1,NULL),
+	   (1,3,'2021-05-17','08:00','09:00',30,1,NULL),
 	   (2,2,'2021-05-18','11:00','13:00',45,1,NULL),
-	   (2,3,'2021-05-19','11:00','13:00',45,1,NULL)
-
+	   (2,3,'2021-05-19','11:00','13:00',45,1,NULL);
--- DB/AdvWebProject.sql
+++ DB/AdvWebProject.sql
@@ -1,38 +1,40 @@
 
 CREATE DATABASE AdvWeb;
 
+USE AdvWeb;
+
 CREATE TABLE HallType(
-	Typeid int NOT NULL IDENTITY(1,1),
+	TypeId int NOT NULL AUTO_INCREMENT,
 	Type varchar(25) NOT NULL,
-	PRIMARY KEY (Typeid)
+	PRIMARY KEY (TypeId)
 );
 
 CREATE TABLE Users(
-	Userid int NOT NULL IDENTITY(1,1),
+	UserId int NOT NULL AUTO_INCREMENT,
 	Name varchar(50) NOT NULL,
 	Surname varchar(50) NOT NULL,
 	Mail varchar(50) NOT NULL,
 	Password varchar(50) NOT NULL,
 	BirthDate Date,
-	PRIMARY KEY (Userid)
+	PRIMARY KEY (UserId)
 );
 
 
 CREATE TABLE SportHalls(
-	Hallid int NOT NULL IDENTITY(1,1),
+	HallId int NOT NULL AUTO_INCREMENT,
 	Type int NOT NULL,
 	WorkFrom Time NOT NULL,
 	WorkTo Time NOT NULL,
 	PriceOneHour decimal NOT NULL,
 	Capacity int,
 	NrHall int NOT NULL,
-	PRIMARY KEY (Hallid),
-	FOREIGN KEY (Type) REFERENCES HallType([Typeid])
+	PRIMARY KEY (HallId),
+	FOREIGN KEY (Type) REFERENCES HallType(TypeId)
 );
 
 
 CREATE TABLE Rentals(
-	Rentalid int NOT NULL IDENTITY(1,1),
+	RentalId int NOT NULL AUTO_INCREMENT,
 	IdUser int NOT NULL,
 	IdHall int NOT NULL,
 	RentalDate date NOT NULL,
@@ -40,8 +42,8 @@ CREATE TABLE Rentals(
 	TimeTo Time NOT NULL,
 	FullPrice decimal NOT NULL,
 	Paid BIT NOT NULL,
-	Description varchar(255)
-	PRIMARY KEY (Rentalid),
-	FOREIGN KEY (IdUser) REFERENCES Users(Userid),
-	FOREIGN KEY (IdHall) REFERENCES SportHalls(Hallid)
+	Description varchar(255),
+	PRIMARY KEY (RentalId),
+	FOREIGN KEY (IdUser) REFERENCES Users(UserId),
+	FOREIGN KEY (IdHall) REFERENCES SportHalls(HallId)
 );
