34fb2984942092c9c3d8dadb78065ad59b1641dd
Alex <nadeykoa@gmail.com>
2021-06-02 22:49:36 +0200

    Added routes. Fixed structrure of project.

---
+++ sport-hall-reservation/src/App.vue
@@ -0,0 +1,25 @@
+
+
+
+<template>
+  <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/reservations">Reservations</router-link> |
+      <router-link to="/about">About</router-link> |
+  </div>
+  <router-view />
+</template>
+
+
+
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ sport-hall-reservation/src/router/index.js
@@ -0,0 +1,32 @@
+
+import { createWebHistory, createRouter } from "vue-router";
+import Home from "@/components/Home.vue";
+import Reservation from "@/components/Reservation.vue";
+import About from "@/components/About.vue"
+// import About from "@/components/About.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/reservations",
+    name: "Reservation",
+    component: Reservation,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
